ab1f3bfb173aff9d975207d6679c58e87da86a50
Swayambhu <swayambhu43@gmail.com>
2022-12-17 18:35:07 -0500

    Path parameters

--- app.py
+++ app.py
@@ -18,5 +18,18 @@ inventory = {
 @app.get("/get-item/{item_id}/{name}")
 async def get_item(item_id: int = Path(None, description="The ID of the Item you would like to view")):
     return inventory[item_id]
+    
+
+@app.get('/blog/unpublished')
+def unpublished():
+    return {'data': 'all unpublished'}
+
+@app.get('/blog/{id}')
+def show(id: int):
+    return {'data': id}
 
 
+
+@app.get('/blog/{id}/comments')
+def comments(id: int):
+    return {'data' : {'1','2'}}
