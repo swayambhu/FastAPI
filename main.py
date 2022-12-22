2c2f92ae2191038d5ccf9494bf0b2024efcc8513
Swayambhu <swayambhu43@gmail.com>
2022-12-22 16:33:50 -0500

    Base model created

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
+from typing import Optional
 from fastapi import FastAPI
+from fastapi.params import Body
+from pydantic import BaseModel
 
 app = FastAPI()
 
@@ -10,6 +13,13 @@ async def root():
 def get_posts():
     return {"data": "This is your posts"}
 
+
+class Post(BaseModel):
+    title: str
+    content: str
+    published: bool = True
+    rating: Optional[int] = None
+    
 @app.post("/createposts")
-def create_posts():
-    return {"message": "succesfully created posts"}
+def create_posts(post: Post):
+    return {"data": post}
