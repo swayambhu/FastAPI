a030ef286966d56018599308e361be7498504a63
Swayambhu <swayambhu43@gmail.com>
2023-01-17 18:21:31 -0500

    cors origin added and updated the requirements file

--- app/main.py
+++ app/main.py
@@ -1,13 +1,23 @@
 from fastapi import FastAPI
-from . import models
 # from .database import engine
 from .routers import post, users, auth, votes
+from fastapi.middleware.cors import CORSMiddleware
+
 
 
 # models.Base.metadata.create_all(engine)
 
 app = FastAPI()
 
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 app.include_router(post.router)
